88c67803112201fbc6e0386133b38d2c001ea232
Michal <00michal@gmail.com>
2022-12-21 09:17:00 +0100

    added rounds functionality

--- rock.js
+++ rock.js
@@ -3,131 +3,146 @@
 //Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 //Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
 
-
-const choices = ['Rock','Paper','Scizors'];
+const choices = ["Rock", "Paper", "Scizors"];
 
 function getComputerChoice() {
-    
-    let choice = choices[Math.floor(Math.random()*3)];
-   
-    return(choice);
+  let choice = choices[Math.floor(Math.random() * 3)];
+
+  return choice;
 }
 
 let playerScore = 0;
 let computerScore = 0;
-const results = document.querySelector('.results');
-const score = document.querySelector('.score');
-results.textContent = '';
-score.textContent = '';
-
+let rounds = 0;
+const results = document.querySelector(".results");
+const restart = document.querySelector(".restart");
+const finalResults = document.querySelector(".finalResults");
+const score = document.querySelector(".score");
+results.textContent = "";
+finalResults.textContent = "";
+score.textContent = "";
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock") {
-        if (computerSelection == "Rock") {
-            results.textContent = ` It's a tie! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-            score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-               
-        } else if (computerSelection == "Scizors") {
-            results.textContent =  `YOU WIN! Your selection ${playerSelection}, Computer chose ${computerSelection}`
-            playerScore++;
-            score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-
-        } else if (computerSelection == "Paper"){
-            results.textContent = `YOU LOSE! Your selection ${playerSelection}, Computer chose ${computerSelection}`
-            computerScore++;
-            score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-        }
+  if (playerSelection == "rock") {
+    if (computerSelection == "Rock") {
+      results.textContent = ` It's a tie! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+      score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    } else if (computerSelection == "Scizors") {
+      results.textContent = `YOU WIN! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+      playerScore++;
+      score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    } else if (computerSelection == "Paper") {
+      results.textContent = `YOU LOSE! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+      computerScore++;
+      score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
     }
-    else if (playerSelection == "paper") {
-        switch(computerSelection) {
-            case "Paper":
-                results.textContent = ` It's a tie! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-                score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-                break;
-            case "Rock":
-                results.textContent = ` You Win! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-                playerScore++;
-                score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-                break;
+    rounds++;
+  } else if (playerSelection == "paper") {
+    switch (computerSelection) {
+      case "Paper":
+        results.textContent = ` It's a tie! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+        score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+        break;
+      case "Rock":
+        results.textContent = ` You Win! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+        playerScore++;
+        score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+        break;
 
-            case "Scizors":
-                results.textContent = ` You Lose! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-                computerScore++;
-                score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-                break;
-        
-        }
-
+      case "Scizors":
+        results.textContent = ` You Lose! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+        computerScore++;
+        score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+        break;
     }
-    else if (playerSelection == "scizors") {
-        switch(computerSelection) {
-            case "Scizors":
-                results.textContent = ` It's a tie! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-                score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-                break;
-            case "Paper":
-                results.textContent = ` You Win! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-                playerScore++;
-                score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-                break;
+    rounds++;
+  } else if (playerSelection == "scizors") {
+    switch (computerSelection) {
+      case "Scizors":
+        results.textContent = ` It's a tie! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+        score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+        break;
+      case "Paper":
+        results.textContent = ` You Win! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+        playerScore++;
+        score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+        break;
 
-            case "Rock":
-                results.textContent = ` You Lose! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
-                computerScore++;
-                score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
-                break;
-        
-        }
-
+      case "Rock":
+        results.textContent = ` You Lose! Your selection ${playerSelection}, Computer chose ${computerSelection}`;
+        computerScore++;
+        score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+        break;
     }
+    rounds++;
   }
- 
+}
 
-  function game() {
+function game() {
   //  for (let i=0; i < 5; i++) {
   //      let playerSelection = prompt('rock paper or scizors?').toLowerCase();
   //      let computerSelection = getComputerChoice();
   //      playRound(playerSelection, computerSelection);
- //   }
+  //   }
 
- //let playerSelection = '';
+  //let playerSelection = '';
 
+  const rock = document.querySelector(".rock");
+  const paper = document.querySelector(".paper");
+  const scizors = document.querySelector(".scizors");
+  const button = document.querySelectorAll("button");
+  let playerSelection = "";
 
- const rock = document.querySelector('.rock');
-  const paper = document.querySelector('.paper');
-  const scizors = document.querySelector('.scizors');
-  const button = document.querySelectorAll('button');
-  let playerSelection = '';
- 
- rock.addEventListener('click', () => {
+  rock.addEventListener("click", () => {
+    if (rounds < 5) {
+      let computerSelection = getComputerChoice();
+      playRound('rock', computerSelection);
+    } else {
+      pickWinner();
+    }
+  });
+
+  paper.addEventListener("click", () => {
+    if (rounds < 5) {
+        let computerSelection = getComputerChoice();
+        playRound('paper', computerSelection);
+      } else {
+        pickWinner();
+      }
+  });
+
+  scizors.addEventListener("click", () => {
+    if (rounds < 5) {
+        let computerSelection = getComputerChoice();
+        playRound('scizors', computerSelection);
+      } else {
+        pickWinner();
+      }
+  });
+
   
-  playerSelection = 'rock';
-  let computerSelection = getComputerChoice();
-  
-  playRound(playerSelection,computerSelection);
- })
+}
 
+function pickWinner() {
+   
+        if (playerScore == computerScore) {
+          finalResults.textContent = "A Tie!";
+        } else if (playerScore > computerScore) {
+            finalResults.textContent = "Player Win";
+        } else {
+            finalResults.textContent = "Computer Win :(";
+        }
+   
+}
 
- paper.addEventListener('click', () => {
-    let computerSelection = getComputerChoice();
-    playRound('paper',computerSelection);
- })
- 
- scizors.addEventListener('click', () => {
-    let computerSelection = getComputerChoice();
-    playRound('scizors',computerSelection);
- })
+restart.addEventListener('click', () =>{
+    playerScore = 0;
+    computerScore = 0;
+    rounds = 0;
+    results.textContent = '';
+    score.textContent ='';
+    finalResults.textContent = '';
+}
+)
 
-
-    if (playerScore == computerScore) {
-        console.log('A Tie!');
-    }
-    else if (playerScore > computerScore) {
-        console.log('Player Win');
-    }
-    else {
-        console.log('Computer Win :(');
-    }
-  }
-
-  game();
+game();
